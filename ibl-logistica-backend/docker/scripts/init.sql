f477fec6c8e34d1f0964c0f57c1754aabf98f18a
Victor Oliveira <ovitor38@gmail.com>
2024-09-18 13:01:49 -0300

    chore(docker): remove docker responsebility seed

--- ibl-logistica-backend/docker/scripts/init.sql
+++ ibl-logistica-backend/docker/scripts/init.sql
@@ -1,14 +1,14 @@
 CREATE TABLE livros (
     id SERIAL PRIMARY KEY,
-    titulo VARCHAR(255) NOT NULL,
-    autor VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
     isbn VARCHAR(13) NOT NULL,
-    quantidade_paginas INT NOT NULL,
-    edicao VARCHAR(50),
-    editora VARCHAR(255)
+    page_count INT NOT NULL,
+    edition VARCHAR(50),
+    publisher VARCHAR(255)
 );
 
-INSERT INTO livros (titulo, autor, isbn, quantidade_paginas, edicao, editora) VALUES
+INSERT INTO livros (title, author, isbn, page_count, edition, publisher) VALUES
 ('Dom Quixote', 'Miguel de Cervantes', '9788535914849', 928, '1ª Edição', 'Companhia das Letras'),
 ('1984', 'George Orwell', '9780451524935', 328, '2ª Edição', 'Penguin Books'),
 ('O Senhor dos Anéis', 'J.R.R. Tolkien', '9780544003415', 1216, '3ª Edição', 'HarperCollins'),
