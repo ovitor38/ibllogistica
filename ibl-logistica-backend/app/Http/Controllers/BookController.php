137cde40474396f75a157338dbae2434e03d2cda
Victor Oliveira <ovitor38@gmail.com>
2024-09-18 16:29:44 -0300

    feat(controller): add book controller and crud methods

--- ibl-logistica-backend/app/Http/Controllers/BookController.php
+++ ibl-logistica-backend/app/Http/Controllers/BookController.php
@@ -2,43 +2,131 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BookRequest;
 use App\Models\Book;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BookController extends Controller
 {
-    public function index(): JsonResponse
+
+    public function show(Book $book): JsonResponse
     {
-        $user = Book::orderBy('id', 'DESC')->get();
 
         return response()->json([
             'status' => true,
-            'users' => $user
-        ],200);
+            'book' => $book
+        ], 200);
     }
 
-    public function show($id)
+    public function store(BookRequest $request): JsonResponse
     {
-        return Book::findOrFail($id);
+
+        DB::beginTransaction();
+
+        try {
+            $book = Book::create($request->all());
+
+            DB::commit();
+
+            return response()->json([
+                'status' => true,
+                'book' => $book,
+                'message' => 'Livro cadastrado com sucesso'
+            ], 201);
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            return response()->json([
+                'status' => false,
+                'message' => 'Houve um erro ao tentar cadastrar esse livro',
+                'error' => $e
+            ], 400);
+        }
     }
 
-    public function store(Request $request)
+    public function update(Request $request, Book $book): JsonResponse
     {
-        $book = Book::create($request->all());
-        return response()->json($book, 201);
+
+        DB::beginTransaction();
+
+        try {
+            $book = Book::findOrFail($book->id);
+            $book->update($request->all());
+            DB::commit();
+
+            return response()->json([
+                'status' => true,
+                'book' => $book,
+                'message' => 'Livro Atualizado com sucesso'
+            ], 200);
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            return response()->json([
+                'status' => false,
+                'message' => 'Houve um erro ao tentar cadastrar esse livro',
+                'error' => $e
+            ], 400);
+        }
     }
 
-    public function update(Request $request, $id)
+    public function destroy(Book $book): JsonResponse
     {
-        $book = Book::findOrFail($id);
-        $book->update($request->all());
-        return response()->json($book, 200);
+
+        try {
+            $book->delete($book);
+
+            return response()->json([
+                'status' => true,
+                'book' => $book,
+                'message' => 'Livro deletado com sucesso'
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Houve um erro ao tentar deletar esse livro',
+                'error' => $e
+            ], 400);
+        }
     }
 
-    public function destroy($id)
+    public function index(Request $request)
     {
-        Book::destroy($id);
-        return response()->json(null, 204);
+        $allowedFields = ['title', 'author', 'isbn', 'page_count', 'edition', 'publisher'];
+
+        $query = $request->only($allowedFields);
+
+        if (empty($query)) {
+            $books = Book::orderBy('id', 'ASC')->get();
+
+            return response()->json([
+                'status' => true,
+                'books' => $books
+            ], 200);
+        }
+
+        $booksQuery = Book::query();
+
+        foreach ($query as $field => $value) {
+            $booksQuery->where($field, 'LIKE', "%{$value}%");
+        }
+
+        $books = $booksQuery->get();
+
+        if ($books->isEmpty()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Nenhum livro encontrado com os parâmetros fornecidos',
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => true,
+            'books' => $books,
+            'message' => 'Livros encontrados com sucesso',
+        ], 200);
     }
 }
--- ibl-logistica-backend/routes/api.php
+++ ibl-logistica-backend/routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
 Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{id}', [BookController::class, 'show']);
+Route::get('/books/{book}', [BookController::class, 'show']);
 Route::post('/books', [BookController::class, 'store']);
-Route::put('/books/{id}', [BookController::class, 'update']);
-Route::delete('/books/{id}', [BookController::class, 'destroy']);
+Route::put('/books/{book}', [BookController::class, 'update']);
+Route::delete('/books/{book}', [BookController::class, 'destroy']);
 
