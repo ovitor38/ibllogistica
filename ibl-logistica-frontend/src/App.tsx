c3666a8e76a492529a115d264aa417494b4dd28f
Victor Oliveira <ovitor38@gmail.com>
2024-09-19 09:14:14 -0300

    feat(app): add react-router-dom

--- ibl-logistica-frontend/src/App.tsx
+++ ibl-logistica-frontend/src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { FunctionComponent } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/home.page'
+import BookDetailsPage from './pages/book-details.page'
+// import BookDetailsPage from './pages/book-details.page'
 
-function App() {
+const App: FunctionComponent = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/book/:id' element={<BookDetailsPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- ibl-logistica-frontend/src/index.tsx
+++ ibl-logistica-frontend/src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
@@ -7,7 +8,7 @@ import reportWebVitals from './reportWebVitals'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 )
 
